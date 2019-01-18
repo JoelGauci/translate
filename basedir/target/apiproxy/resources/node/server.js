948bcf76a6df1e8fc1aa7af8d242104ca91b2f41
Joel Gauci <joelgauci@joelgauci-macbookpro.roam.corp.google.com>
2019-01-18 11:21:13 +0100

    deletion of target deployment directory

--- basedir/target/apiproxy/resources/node/server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write('{"data":{"translations":[{"translatedText":"mock data received"}]}}'); //write a response to the client
-    res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
